32eaca15ceec6d8eb7f057931b6a6a701d84a037
Priska Sudheer <45616994+PriskaSudheer@users.noreply.github.com>
2018-12-07 15:46:36 +1100

    Add files via upload
    Created a new component called servercomponent

---
+++ server.component.ts
@@ -0,0 +1,12 @@
+import{ Component } from '@angular/core';
+import { Server } from 'selenium-webdriver/safari';
+
+
+@Component({
+    selector:'app-server',
+    templateUrl:'./Server.component.html'
+})
+export class ServerComponent{
+
+
+}
