a1b3c2709b4cb3f290a1c88b224c01e7a162b5c4
Priska Sudheer <45616994+PriskaSudheer@users.noreply.github.com>
2018-12-10 15:32:52 +1100

    Event Binding

--- servers.component.ts
+++ servers.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { createDecipher } from 'crypto';
 
 @Component({
   //selector:' [app-servers]',
@@ -12,6 +13,7 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
 
   allowNewServer=false;
+  ServerCreationStatus="No Server Created";
   constructor() {
     setTimeout( ()=>{
       this.allowNewServer=true;
@@ -19,6 +21,9 @@ export class ServersComponent implements OnInit {
    }
 
   ngOnInit() {
+  }
+  onCreateServer(){
+    this.ServerCreationStatus="Server created";
   }
 
 }
