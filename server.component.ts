2cb0556694df7f50c3f93b31a35c633fb6917656
Priska Sudheer <45616994+PriskaSudheer@users.noreply.github.com>
2018-12-10 12:32:43 +1100

    Data Binding: String Interpolation

--- server.component.ts
+++ server.component.ts
@@ -1,12 +1,16 @@
-import{ Component } from '@angular/core';
-import { Server } from 'selenium-webdriver/safari';
+import { Component } from "@angular/core";
 
 
 @Component({
-    selector:'app-server',
-    templateUrl:'./Server.component.html'
+selector:'app-server',
+templateUrl:'./server.component.html'
+
 })
 export class ServerComponent{
+ ServerID :number= 10;
+ ServerStatus :String= "offline";
 
-
+ getServerStatus(){
+     return this.ServerStatus;
+ }
 }
