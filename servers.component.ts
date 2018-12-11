b06471a75936455fec80f4fb37f049e09a3ae99c
Priska Sudheer <45616994+PriskaSudheer@users.noreply.github.com>
2018-12-11 13:17:21 +1100

    Passing and using data with event binding

--- servers.component.ts
+++ servers.component.ts
@@ -1,29 +1,36 @@
 import { Component, OnInit } from '@angular/core';
-import { createDecipher } from 'crypto';
+
 
 @Component({
   //selector:' [app-servers]',
   selector: 'app-servers',
- // template: `
+  // template: `
   //<app-server></app-server>
   //<app-server></app-server>`,
-  templateUrl:'./servers.component.html',
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
 
-  allowNewServer=false;
-  ServerCreationStatus="No Server Created";
+  allowNewServer = false;
+  ServerCreationStatus = "No Server Created";
+  serverName='';
+
   constructor() {
-    setTimeout( ()=>{
-      this.allowNewServer=true;
-    },2000);
-   }
+    setTimeout(() => {
+
+      this.allowNewServer = true;
+    }, 2000);
+  }
 
   ngOnInit() {
+
   }
-  onCreateServer(){
-    this.ServerCreationStatus="Server created";
+  onCreateServer() {
+    this.ServerCreationStatus = "Server created";
+  }
+  onUpdateServerName(event: Event) {
+ this.serverName=(<HTMLInputElement>event.target).value;
   }
 
 }
